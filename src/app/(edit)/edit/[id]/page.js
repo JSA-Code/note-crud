eaafbe7132a7179302792486a7856dfc38967872
JSA-Code <jsafortech+github@gmail.com>
2023-08-21 14:00:10 -0700

    added edit note functionality

--- src/app/(edit)/edit/[id]/page.js
+++ src/app/(edit)/edit/[id]/page.js
@@ -1,5 +1,22 @@
 import EditNoteForm from "../../EditNoteForm";
 
-export default function EditNote() {
-  return <EditNoteForm />;
+const getNoteById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/notes/${id}`, {
+      cache: "no-store",
+    }); // cache: "no-store" to prevent caching
+    if (!res.ok) {
+      throw new Error(`Failed to fetch note. Error: ${res.status}`);
+    }
+    return res.json();
+  } catch (error) {
+    console.log(`Error while loading note, ${error}`);
+  }
+};
+
+export default async function EditNote({ params }) {
+  const { id } = params;
+  const { note } = await getNoteById(id);
+  const { title, description } = note;
+  return <EditNoteForm id={id} title={title} description={description} />;
 }
--- src/app/api/notes/[id]/route.js
+++ src/app/api/notes/[id]/route.js
@@ -1,20 +1,18 @@
-import { connectMongoDB, connection } from "@/libs/mongodb";
+import { connectMongoDB } from "@/libs/mongodb";
 import Note from "@/models/note";
 import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const note = await Note.findById(id);
+  return NextResponse.json({ note }, { status: 200 });
+}
 
 export async function PUT(request, { params }) {
   const { id } = params;
   const { newTitle: title, newDescription: description } = await request.json();
   await connectMongoDB();
   await Note.findByIdAndUpdate(id, { title, description });
-  await connection.close();
   return NextResponse.json({ message: "NOTE UPDATED!" }, { status: 200 });
-}
-
-export async function GET(request, { params }) {
-  const { id } = params;
-  await connectMongoDB();
-  const note = await Note.findById(id);
-  await connection.close();
-  return NextResponse.json({ note }, { status: 200 });
 }
